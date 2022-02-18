c16bbb519c2796dd285f919076a25cf8b7fa915c
Lukáš Skřivánek <luky.skrivos@gmail.com>
2022-02-18 10:26:41 +0100

    fixes

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import sys
+import csv
 
-url: str = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=3&xnumnuts=3101"
+
 hostname: str = "https://volby.cz/pls/ps2017nss/"
+url = sys.argv[1]
+name_of_city = sys.argv[2]
 
 complete_url_array = []
 municipal_codes = []
@@ -19,7 +22,7 @@ def get_data(url_adress):
     r = requests.get(url_adress)
     status = True
     while status:
-        if (r.status_code != 200):
+        if r.status_code != 200:
             print("Status code is not 200")
         else:
             status = False
@@ -29,14 +32,15 @@ def get_data(url_adress):
     return soup, table
 
 
-def get_numbers(soup, list_numbers):
+def get_numbers():
     for numbers in table:
         for number in numbers.find_all("td", class_="cislo"):
             municipal_codes.append(number.text)
+
     return municipal_codes
 
 
-def get_city(soup, list_cities):
+def get_city(soup):
     table = soup.find_all("tr")
     for cities in table:
         for city in cities.find_all("td", headers="t1sa1 t1sb2"):
@@ -69,16 +73,16 @@ def get_data_for_properties():
     return complete_url_array
 
 
-def get_other_properties(soup, complete_url_array):
+def get_other_properties(complete_url_array):
     for i in complete_url_array:
         r = requests.get(i)
         soup = BeautifulSoup(r.text, "html.parser")
         voter = soup.find("td", {"headers": "sa2"}).text
-        voter_list.append(voter.replace("xa", ""))
+        voter_list.append(voter.replace("xa", "").replace(" ", ""))
         envelopes = soup.find("td", {"headers": "sa5"}).text
-        issued_envelopes.append(envelopes)
+        issued_envelopes.append(envelopes.replace(" ", ""))
         votes = soup.find("td", {"headers": "sa6"}).text
-        valid_votes.append(votes)
+        valid_votes.append(votes.replace(" ", ""))
     return voter_list, issued_envelopes, valid_votes
 
 
@@ -87,37 +91,48 @@ def get_list_of_parties():
         r = requests.get(tr)
         soup = BeautifulSoup(r.text, "html.parser")
         for fronde in soup.find_all("td", class_="overflow_name"):
-            if (fronde != "-"):
+            if fronde != "-":
                 list_of_parties.append(fronde.text)
-        if (len(list_of_parties) > 26):
+        if len(list_of_parties) > 26:
             break
     return list_of_parties
 
 
+
 def get_to_csv(list_of_parties):
-    dict = {"municipal_code": municipal_codes,
-            "names of the village": names_of_the_village,
-            "valid votes": valid_votes,
-            "voter list": voter_list,
-            "issued nevelopes": issued_envelopes,
-            tuple(list_of_parties): "",
-            }
-    df = pd.DataFrame(dict)
-    df.to_csv("Data.csv")
+    csv_colums = ["municipal code", "names of the village", "valid votes",
+                  "voter list", "issued envelopes", tuple(list_of_parties)]
+    dict_of_data = [{"municipal code": municipal_codes},
+                    {"names of the village": names_of_the_village},
+                    {"valid votes": valid_votes},
+                    {"voter list": voter_list},
+                    {"issued envelopes": issued_envelopes},
+                    {tuple(list_of_parties): ""}]
+
+    with open("CeskeBudejovice.csv", "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = csv_colums)
+        writer.writeheader()
+        writer.writerows(dict_of_data)
 
 
 def main():
-    print("I am donwloading data from this URL", url)
-    get_data(url)
-    get_numbers(soup, municipal_codes)
-    get_city(soup, names_of_the_village)
-    get_data_for_properties()
-    get_other_properties(soup, complete_url_array)
-    print("I am saving, to file Data.csv")
-    get_list_of_parties()
-    get_to_csv(list_of_parties[0:25])
-    print("I am ending this program")
+    if url.startswith("https://volby.cz/pls/ps2017nss/"):
+        print("I am donwloading data from this URL", url)
+        get_data(url)
+        get_numbers()
+        get_city(soup)
+        get_data_for_properties()
+        get_other_properties(complete_url_array)
+        print("I am saving, to file ", sys.argv[2])
+        get_list_of_parties()
+        get_to_csv(list_of_parties[0:25])
+        print("I am ending this program")
+    else:
+        print("Wrong data")
 
 
 if __name__ == '__main__':
     main()
+
+
+
